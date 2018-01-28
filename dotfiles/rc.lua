20424a6b913a4649d84913617abec3fee2346003
quapka <quapka@gmail.com>
2018-01-28 16:29:53 +0100

    Set terminal and editor defaults

--- dotfiles/rc.lua
+++ dotfiles/rc.lua
@@ -547,3 +547,7 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+--
+
+terminal = 'xterm'
+editor = os.getenv('EDITOR') or 'neovim'
