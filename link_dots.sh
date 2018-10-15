3bfa2105e06b3551e9c32503df3f8ab6bdb597d3
quapka <quapka@gmail.com>
2018-10-15 10:54:39 +0200

    Allow overwriting dotfiles and make a backup

--- link_dots.sh
+++ link_dots.sh
@@ -8,10 +8,33 @@ fi
 
 # kudos to url: https://stackoverflow.com/a/246128/2377489
 dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+backup="./backup"
 
 for dfile in .bashrc .vimrc .vundle_conf.vim .bash_aliases .profile .Xresources .tmux.conf; do
+    # TODO make this logic into a function, that just accepts some paths,
+    # because it needs to be used for rc.lua as well.
     echo "Trying to create a link for $dfile:"
-    ln -s "$dotfiles/$dfile" "$HOME"
+
+    force=""
+    existing_dotfile="$HOME/$dfile"
+    if [ -e "$existing_dotfile" ]; then
+        echo "The file $existing_dotfile already exists."
+        read -p "Would you like to overwrite it (it will be saved in ./backup) [y/n]?" -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            force="--force"
+            if [ ! -d "$backup" ]; then
+                mkdir "$backup"
+            fi
+            echo "Copying $existing_dotfile to $backup"
+            cp "$existing_dotfile" "$backup/$dfile.$( date +"%Y%m%d-%H%M-%S")"
+        else
+            echo "Skipping $existing_dotfile"
+            continue
+        fi
+    fi
+    echo "Creating the symlink $existing_dotfile"
+    ln --symbolic "$force" "$dotfiles/$dfile" "$HOME"
     if [ $? -eq "0" ]; then
         echo "OK"
     else
