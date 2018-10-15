f1586504bf7e03a6b05e888bf77f4632a00f5c2e
quapka <quapka@gmail.com>
2018-10-15 10:15:22 +0200

    Update the with rc.lua and be more echoy

--- link_dots.sh
+++ link_dots.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ -n "$HOME" ]; then
     echo "Using HOME=$HOME"
-    dir="$HOME"
 else
     echo "Could not find HOME. Please, set HOME and try again."
     exit 1
@@ -9,7 +8,22 @@ fi
 
 # kudos to url: https://stackoverflow.com/a/246128/2377489
 dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo "$dotfiles"
+
 for dfile in .bashrc .vimrc .vundle_conf.vim .bash_aliases .profile .Xresources .tmux.conf; do
-    ln -s "$dotfiles/$dfile" "$dir"
+    echo "Trying to create a link for $dfile:"
+    ln -s "$dotfiles/$dfile" "$HOME"
+    if [ $? -eq "0" ]; then
+        echo "OK"
+    else
+        echo "ERROR"
+    fi
+    echo
 done
+
+echo "Trying to create a link for rc.lua:"
+ln -s "$dotfiles/rc.lua" "$HOME/.config/awesome/"
+if [ $? -eq "0" ]; then
+    echo "OK"
+else
+    echo "ERROR"
+fi
